6fd5a46453df548ff73baaf7ee7e95a90386e72c
Julio Costa <julinho.colorado@gmail.com>
2022-02-01 04:21:51 -0300

    'Funcional'

--- sistex/urls.py
+++ sistex/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from sistex.base.views import home
+from django.urls import path, include
+from sistex.base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', views.home, name='home'),
 
 ]
