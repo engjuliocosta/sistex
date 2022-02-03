f10cff9cd7d37290c637337d42e534e24fac9946
Julio Costa <julinho.colorado@gmail.com>
2022-02-03 02:42:21 -0300

    Configuração para insolamento de variáveis de ambientes com Decouple

--- sistex/settings.py
+++ sistex/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^g8l5(3xpndwwsl&t)^-zdr36x_!q8@4th%h@f5-v-(ue_6%h4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ["*"]
 
